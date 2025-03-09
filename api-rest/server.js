4cfda6d9c0db752ac915c5bcc461f8679e27ea5b
Pipex02 <ospinoospino4@gmail.com>
2025-03-09 13:19:54 -0500

    Change: Improved MySQL connection handling & NGINX configuration for EC2
    - Enhanced error handling in server.js by adding 'return' to prevent unnecessary execution.
    - Updated nginx.conf to ensure proper functionality on EC2.

--- api-rest/server.js
+++ api-rest/server.js
@@ -2,16 +2,15 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 
-
 const app = express();
 const PORT = 3000;
 
 // Configuración de CORS para permitir peticiones desde el frontend
 app.use(cors());
 
-// Configuración de conexión a MySQL
+// Configuración de conexión a MySQL en RDS
 const db = mysql.createConnection({
-    host: "localhost",
+    host: "xxxxxx",
     user: "xxxxxx",
     password: "xxxxxx",
     database: "xxxxxx"
@@ -21,12 +20,12 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) {
         console.error("❌ Error al conectar a MySQL:", err);
-    } else {
-        console.log("✅ Conectado a MySQL");
+        return;
     }
+    console.log("✅ Conectado a MySQL en RDS");
 });
 
-// Ruta para obtener la última coordenada a traves de la API rest
+// Ruta para obtener la última coordenada a traves de la API REST
 app.get("/coordenadas", (req, res) => {
     const query = "SELECT latitud, longitud, timestamp FROM coordenadas ORDER BY id DESC LIMIT 1";
 
