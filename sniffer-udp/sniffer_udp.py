4d0e6b4cb3ff55051fde8fdb6b66bc630ce0a765
AllieSalom <allie81194@gmail.com>
2025-04-28 13:07:28 -0500

    Remove fix comments from code

--- sniffer-udp/sniffer_udp.py
+++ sniffer-udp/sniffer_udp.py
@@ -10,7 +10,7 @@ def conectar_bd():
         conexion = pymysql.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),  # Cambia por tu contraseña
+            password=os.getenv("DB_PASSWORD"),  
             database=os.getenv("DB_NAME")
         )
         return conexion
